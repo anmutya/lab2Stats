66f73d95db5958bd3ed48b57c23dea4317fcdcc0
anmutya <mutovkina.ania@gmail.com>
2024-04-11 11:18:23 +0300

    Неважно сколько выборок

--- lab2Stats/src/main/java/com/mycompany/lab2stats/ExcelProvider.java
+++ lab2Stats/src/main/java/com/mycompany/lab2stats/ExcelProvider.java
@@ -25,7 +25,7 @@ public class ExcelProvider {
     public ArrayList<ArrayList<Double>> readFromXLSX(String path) throws IOException {
         ArrayList<ArrayList<Double>> data = new ArrayList<>();
         try (XSSFWorkbook myExcelFWorkbook = new XSSFWorkbook(path)) {
-            XSSFSheet myExcelSheet = myExcelFWorkbook.getSheet("Вариант 1");
+            XSSFSheet myExcelSheet = myExcelFWorkbook.getSheetAt(0);
 
             int numRows = myExcelSheet.getPhysicalNumberOfRows();
             int numCols = myExcelSheet.getRow(0).getLastCellNum();
@@ -53,30 +53,30 @@ public class ExcelProvider {
     public void expotExcel(ArrayList<ArrayList<Double>> stats) throws IOException{
         XSSFWorkbook wb = new XSSFWorkbook();
         XSSFSheet sheet = wb.createSheet("Calculations");
-        craateTitles(sheet);
-        addStatsToRow(sheet.getRow(1), stats.get(0));
-        addStatsToRow(sheet.getRow(2), stats.get(1));
-        addStatsToRow(sheet.getRow(3), stats.get(2));
+        craateTitles(sheet, stats);
+        for(int i = 1; i <= stats.size(); i++){
+            addStatsToRow(sheet.getRow(i), stats.get(i-1));
+        }
         XSSFSheet sheet2 = wb.createSheet("Covariance Matrix");
         covarianceMatrix(sheet2, stats);
         try {
-            wb.write(new FileOutputStream(new File("/Users/annamutovkina/Downloads/Calculations.xlsx")));
+            wb.write(new FileOutputStream(new File("./Calculations.xlsx")));
             wb.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ExcelProvider.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    private void craateTitles(XSSFSheet sheet){
-        XSSFRow row = sheet.createRow(0);
-        XSSFRow row1 = sheet.createRow(1);
-        XSSFRow row2 = sheet.createRow(2);
-        XSSFRow row3 = sheet.createRow(3);
-        row1.createCell(0).setCellValue("X");
-        row2.createCell(0).setCellValue("Y");
-        row3.createCell(0).setCellValue("Z");
+    private void craateTitles(XSSFSheet sheet, ArrayList<ArrayList<Double>> stats){
+
+        for(int i = 1; i <= stats.size() ; i++){
+            XSSFRow row = sheet.createRow(i);
+            XSSFCell cell = row.createCell(0);
+            cell.setCellValue("Выборка " + i);
+        }
         String[] headers = new String[]{"Geometric Mean", "Mean", "Standard Deviation", 
             "Sample Size", "Number of Items", "Variance Coefficient", "Lower Confidence Bound", "Upper Confidence Bound", 
             "Variance", "Maximum", "Minimum"};
+        XSSFRow row = sheet.createRow(0);
         for (int i = 1; i < headers.length + 1; i++) {
             row.createCell(i).setCellValue(headers[i - 1]);
         } 
@@ -88,17 +88,16 @@ public class ExcelProvider {
         }
    }
    private void covarianceMatrix(XSSFSheet sheet, ArrayList<ArrayList<Double>> stats){
-       String[] headers = new String[]{"X", "Y", "Z"};
        XSSFRow row = sheet.createRow(0);
-       for (int i = 1; i <= headers.length; i++) {
-           sheet.createRow(i).createCell(0).setCellValue(headers[i - 1]);
+       for (int i = 1; i <= stats.size(); i++) {
+           sheet.createRow(i).createCell(0).setCellValue("Выборка "+ i);
        }
-       for (int i = 1; i <= headers.length; i++) {
-           row.createCell(i).setCellValue(headers[i - 1]);
+       for (int i = 1; i <= stats.size(); i++) {
+           row.createCell(i).setCellValue("Выборка " + i);
        }
-       for(int i = 1; i <= headers.length; i++){
+       for(int i = 1; i <= stats.size(); i++){
            int arrayNumber = i-1;
-           for(int j = 1; j<= headers.length; j++){
+           for(int j = 1; j<= stats.size(); j++){
                int indexInArrayNumber = 10 + j;
                sheet.getRow(i).createCell(j).setCellValue(stats.get(arrayNumber).get(indexInArrayNumber));
            }
